50557d008acddd3c93b641dc07f37966bd9f30e3
prior93 <Parashar.adhikary@gmail.com>
2021-04-20 13:32:32 +0300

    Apple Watch Gauge

--- SwiftUIGauge WatchKit Extension/ContentView.swift
+++ SwiftUIGauge WatchKit Extension/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    var gradient = Gradient(colors: [.red,.pink,.purple,.green])
+    
+    
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        Gauge(value: 33.0, in : 0.0 ... 100.0) {
+            Text("Speed").foregroundColor(.blue)
+        } currentValueLabel:{
+            Text("33") .foregroundColor(.blue)
+        }
+        minimumValueLabel: {
+            Text("0").foregroundColor(.pink)
+        }
+        maximumValueLabel:{
+            Text("100").foregroundColor(.green)
+        }
+        .gaugeStyle(CircularGaugeStyle(tint: gradient))
+
     }
 }
 
