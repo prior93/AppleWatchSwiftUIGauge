30625d9be9434a6c9363c1160295ce7d71ccb359
prior93 <Parashar.adhikary@gmail.com>
2021-04-20 13:16:49 +0300

    Initial Commit

---
+++ SwiftUIGauge WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIGauge WatchKit Extension
+//
+//  Created by parashar.r.adhikary on 20/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIGauge WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  SwiftUIGauge WatchKit Extension
+//
+//  Created by parashar.r.adhikary on 20/04/2021.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
